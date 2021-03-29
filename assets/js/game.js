6c4b534d9697233b59dfa53928caf88109e1be05
Nathanael White <narhanaelw99@gmail.com>
2021-03-28 23:27:52 -0500

    added enemy while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,9 +13,12 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+   // Alert players that they are starting the round
+   window.alert("welcome to Robot Gladiators!");
+
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
@@ -64,8 +67,11 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to chose a valid option. Try again!");
     }
+    } 
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
