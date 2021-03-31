cf5de30e4f466a689c0f9ac620dbf595b8bb1f9d
Nathanael White <narhanaelw99@gmail.com>
2021-03-30 23:28:52 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,21 +68,46 @@ var fight = function(enemyName) {
     } 
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
+// function to start new game
+var startGame = function() {
+    // // reset player stats 
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;            
+            fight(pickedEnemyName);
+            } 
+        else {
+            window.alert("You have lost you robot in battle! Game Over!");
+            break;
+        }   
+    }
+    endGame();
+};
+
+// function to end game
+var endGame = function() {
+    window.alert("the game has now ended. let's see how you did!")
+    // if player is still alive
     if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("You've lost your robot in battle.");
+    }
+     // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
 
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+    if (playAgainConfirm) {
+     // restart game
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
 
-        var pickedEnemyName = enemyNames[i];
-
-        enemyHealth = 50;
-        
-        fight(pickedEnemyName);
-    } 
-    
-    else {
-        window.alert("You have lost you robot in battle! Game Over!");
-        break;
-    }    
-}
+startGame();
