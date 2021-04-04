1b03cbb8d62853830b684b800d8d38e65fb00ecd
Nathanael White <narhanaelw99@gmail.com>
2021-04-03 21:46:26 -0500

    fixed fight/skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,25 +6,38 @@ var randomNumber = function (min, max) {
     return value;
 };
 
-var fight = function(enemy) {
-    while(playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
+var fightOrSkip = function() {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm play wants to skip the fight
+    if (promptFight === "" || promptFight === null) {
+        window.alert("you need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-        // if yes (true), leave fight
         if (confirmSkip) {
-            window.alert(playerInfo.name + " has chosen to skip this fight. Goodbye!");
-            // subtract money from playerInfo.money for skipping
-            playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerInfo.money", playerInfo.money);
-            break;
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+
+            playerInfo.playerMoney = playerInfo.money - 10;
+            
+            // return true if the player wants to leave
+            return true;
         }
     }
+    return false;
+}
+
+var fight = function(enemy) {
+    while(playerInfo.health > 0 && enemy.health > 0) {
+    
+        if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
+            break;
+        }
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -159,7 +172,6 @@ var getPlayerName = function() {
 }
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
     name: getPlayerName(),
     health: 100,
     attack: 10,
